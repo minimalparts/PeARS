f82c659e8b4d4cb2dfd43db1259b381f844c84cf
Stultus <hrishi.kb@gmail.com>
2015-06-24 21:24:12 +0530

    initial setup for flask app

---
+++ config.py
@@ -0,0 +1,10 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+import os
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+# db configurations - not using now
+SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
+SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')
+CSRF_ENABLED = True
+SECRET_KEY='parayan-manasilla'
---
+++ run.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python
+from app import app
+print "PeARS is starting...",
+app.run(debug=True)
+print "done"
+
+
