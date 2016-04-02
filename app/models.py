54b0a7375bff107b6945e85702cddc03d1ef911a
minimalparts <minimalparts@users.noreply.github.com>
2016-04-02 11:52:33 +0200

    More replacing wikiwoods

--- app/models.py
+++ app/models.py
@@ -1,7 +1,7 @@
 from app import db
 
-class WikiWoods(db.Model):
-    __bind_key__ = 'wikiwoods'
+class OpenVectors(db.Model):
+    __bind_key__ = 'openvectors'
     id = db.Column(db.Integer, primary_key=True)
     word =db.Column(db.UnicodeText(64))
     vector = db.Column(db.Text)
